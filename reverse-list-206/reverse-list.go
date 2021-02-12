1c1571dd927a9534ae7af055d8692e7ef9dfd595
mingmeng <i@yangruixin.com>
2021-02-12 23:27:22 +0800

    feat(reverse list): add recur and iterate reversing list(lc-206)

---
+++ reverse-list-206/reverse-list.go
@@ -0,0 +1,34 @@
+package reverse_list_206
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 递归方式链表反转
+func reverseListRecur(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	node := reverseListRecur(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return node
+}
+
+// 迭代方式链表反转
+func reverseListIterate(head *ListNode) *ListNode {
+	var pre *ListNode
+	now := head
+
+	for now != nil {
+		next := now.Next
+
+		now.Next = pre
+		pre = now
+		now = next
+	}
+
+	return pre
+}
