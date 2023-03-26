3c71e607687d41484f574ce3b3db5ef40c2713e0
mingmeng <i@yangruixin.com>
2023-03-26 14:33:22 +0800

    feat: add 844 165

--- string/backspace-844.go
+++ string/backspace-844.go
@@ -1,32 +1,34 @@
-package main
+package string
 
-//func backspaceCompare(s string, t string) bool {
-//	fmt.Print(backspace(s))
-//	fmt.Print(backspace(t))
-//	return backspace(s) == backspace(t)
-//}
-//
-//func backspace(s1 string) string {
-//	var slow = -1
-//	var fast = 0
-//
-//	s := []byte(s1)
-//
-//	for fast < len(s) {
-//		if s[fast] == '#' {
-//			if slow < 0 {
-//				slow = -1
-//			} else {
-//				slow -= 1
-//			}
-//		} else {
-//			s[slow+1] = s[fast]
-//			slow += 1
-//		}
-//		fast += 1
-//	}
-//	if slow == -1 {
-//		return ""
-//	}
-//	return string(s[:slow])
-//}
+import "fmt"
+
+func backspaceCompare(s string, t string) bool {
+	fmt.Println(backspace(s))
+	fmt.Println(backspace(t))
+	return backspace(s) == backspace(t)
+}
+
+func backspace(s1 string) string {
+	var slow = 0
+	var fast = 0
+
+	s := []byte(s1)
+
+	for fast < len(s) {
+		if s[fast] == '#' {
+			if slow < 0 {
+				slow = -1
+			} else {
+				slow -= 1
+			}
+		} else {
+			s[slow+1] = s[fast]
+			slow += 1
+		}
+		fast += 1
+	}
+	if slow == -1 {
+		return ""
+	}
+	return string(s[:slow])
+}
